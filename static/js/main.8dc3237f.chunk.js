654fbca153abe687d1c8c9b8f2a628ecb8f7909f
xSmokeAndMirrorsx <46545831+xSmokeAndMirrorsx@users.noreply.github.com>
2021-09-30 17:42:45 -0400

    Updates

--- static/js/main.8dc3237f.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpmy-react-app"]=this["webpackJsonpmy-react-app"]||[]).push([[0],{10:function(e,t,c){"use strict";c.r(t);c(1);var n=c(3),r=c.n(n),s=(c(8),c(0));var a=function(){return Object(s.jsx)("div",{className:"App",children:Object(s.jsx)("h1",{children:"Yo!"})})};r.a.render(Object(s.jsx)(a,{}),document.getElementById("root"))},8:function(e,t,c){}},[[10,1,2]]]);
-//# sourceMappingURL=main.8dc3237f.chunk.js.map
